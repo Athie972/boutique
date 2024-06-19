b94af324955d4e142922eaa42dfb0f1e3dc1e787
Athie972 <athena.martiny@gmail.com>
2024-06-19 12:11:23 -0400

    création des routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,19 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use Faker\Guesser\Name;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('products.produits');
-});
 
+// afficher les produits
 Route::get('/', [ProductController::class, 'index'])->name('product.index');
+
+// créer un nouveau produit
+Route::post('/products', [ProductController::class, 'store'])->name('product.store');
+
+//modifier un produit
+Route::get('/edit/{id}', [ProductController::class, 'edit'])->name('product.edit');
+Route::post('/update/{id}', [ProductController::class,'update'])->name('product.update');
+
+//supprimer un produit
+Route::post('/delete/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
