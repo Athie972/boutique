18eb04a328a9659ed57ceb9649753041011d49b8
Athie972 <athena.martiny@gmail.com>
2024-06-24 06:53:59 -0400

    installation breeze

--- resources/views/produit/modifier.blade.php
+++ resources/views/produit/modifier.blade.php
@@ -4,18 +4,18 @@
 <form action="{{ route('product.update', ['id' => $product->id]) }}" method="POST">
     @csrf
     <div class="mb-6">
-        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom</label>
-        <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="{{$product->name}}" required>
+        <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Nom</label>
+        <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value="{{$product->name}}" required>
     </div>
     <br>
     <div class="mb-6">
-        <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-        <input type="text" name="description" id="description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="{{$product->description}}" required>
+        <label for="description" class="block mb-2 text-sm font-medium text-gray-900">Description</label>
+        <input type="text" name="description" id="description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value="{{$product->description}}" required>
     </div>
     <br>
     <div class="mb-6">
-        <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Prix</label>
-        <input type="text" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="{{$product->price}}" required>
+        <label for="price" class="block mb-2 text-sm font-medium text-gray-900">Prix</label>
+        <input type="text" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value="{{$product->price}}" required>
     </div>
     <br>
     <button type="submit" class="btn btn-primary">Modifier</button>
